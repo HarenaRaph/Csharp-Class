295710ecfd78f3506e05cd294fdbef1f156cbb68
Harena Raphaël <156826743+HarenaRaph@users.noreply.github.com>
2025-05-03 23:18:26 +0300

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+
+namespace Devoir1
+{
+    class Point
+    {
+        private int X { get; set; }
+        private int Y { get; set; }
+
+
+        public Point( int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public Point(int X):this(X, 0) {}
+        public Point(): this(0, 0) {}
+
+        public void Nord()
+        {
+            if(this.Y > 0)
+            {
+                this.Y--;
+            }
+        }
+        
+        public void Sud()
+        {
+            if(this.Y < 10)
+            {
+                this.Y++;
+            }
+        }
+
+        public void Est()
+        {
+            if (this.X < 10 )
+            {
+                this.X++;
+            }
+        }
+
+        public void Ouest()
+        {
+            if (this.X > 0)
+            {
+                this.X--;
+            }
+        }
+
+        public string directionX( int X_objectif)
+        {
+            int difference = this.X - X_objectif;
+            string direction ="";
+            if (difference != 0)
+            {
+                if(difference < 0)
+                {
+                    for(int i = 0; i < Math.Abs(difference); i++)
+                    {
+                        this.Est();
+                        Console.WriteLine(this.X + " " + this.Y);
+                        direction += "E";
+                    }
+                }else
+                {
+                    for (int i = 0; i < difference; i++)
+                    {
+                        this.Ouest();
+                        Console.WriteLine(this.X + " " + this.Y);
+                        direction += "O";
+                    }
+                }
+            }
+            return direction;
+        }
+
+        public string directionY( int Y_objectif)
+        {
+            int difference = this.Y - Y_objectif;
+            string direction = "";
+            if (difference != 0)
+            {
+                if (difference < 0)
+                {
+                    for (int i = 0; i < Math.Abs(difference); i++)
+                    {
+                        this.Sud();
+                        Console.WriteLine(this.X + " " + this.Y);
+                        direction += "S";
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < difference; i++)
+                    {
+                        this.Nord();
+                        Console.WriteLine(this.X + " " + this.Y);
+                        direction += "N";
+                    }
+                }
+            }
+            return direction;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+
+namespace Devoir1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Point p = new(x, y);
+
+            Console.Write("x_objectif: ");
+            int x_objectif = int.Parse(Console.ReadLine());
+            Console.Write("y_objectif: ");
+            int y_objectif = int.Parse(Console.ReadLine());
+
+            string directionX = p.directionX(x_objectif);
+            string directionY = p.directionY(y_objectif);
+
+            Console.WriteLine("Chemin: " + directionX + directionY);
+
+            Console.WriteLine("Entrez les directions (N, S, E, P");
+            string direction = Console.ReadLine().ToUpper();
+
+            for (int i = 0; i < direction.Length; i++)
+            {
+                switch (direction[i])
+                {
+                    case 'N': 
+                        p.Nord();
+                        Console.WriteLine($"x: {p.x} y: {p.y}"); 
+                        break;
+                    case 'S':
+                       p.Sud();
+                        Console.WriteLine($"x: {p.x} y: {p.y}"); 
+                        break;
+                    case 'E': 
+                        p.Est();
+                        Console.WriteLine($"x: {p.x} y: {p.y}"); 
+                        break;
+                    case 'O': 
+                       p.Ouest();
+                        Console.WriteLine($"x: {p.x} y: {p.y}"); 
+                        break;
+                    default: Console.WriteLine("Mauvaises directives"); break;
+                }
+
+            }
+
+            Console.WriteLine($"Position finale: x: {p.x} y: {p.y}");
+
+        }
+
+    }
+}
